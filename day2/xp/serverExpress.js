2e37fd4deda04ba7ab3462623e0e3b8749c85384
avivketter260 <55919735+avivketter260@users.noreply.github.com>
2021-05-24 21:23:23 +0300

    Delete day2 directory

--- day2/xp/serverExpress.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.send('<h1>tagit</h1>'))
-
-app.listen(3001)
--- day2/xp2/script.js
+++
@@ -1,8 +0,0 @@
-console.log('sfsdf')
-fetch(`http://localhost:3001`)
-.then((response) => {
-    //console.log(response);
-    response.json().then((data) => {
-        console.log('Object:', data);
-    });
-});
